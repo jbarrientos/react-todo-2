c5ca0786900c3172b18911ffb857cf88102ee040
Jorge Barrientos <jorge.barrientos@gmail.com>
2017-07-14 19:25:46 -0600

    Add filter funcionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -27,8 +27,22 @@ module.exports = {
     });
 
     // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      return searchText.length === 0 ||
+      todo.text.toLowerCase().indexOf(searchText) > -1;
+    });
 
     // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      // a non-completed && b completed
+      if(!a.completed && b.completed){
+        return -1;
+      }else if(a.completed && !b.completed){
+        return 1;
+      }else {
+        return 0;
+      }
+    });
 
     return filteredTodos;
   }
