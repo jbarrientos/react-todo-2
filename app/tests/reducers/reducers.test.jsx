c3cdbc39e6ad6f5c3f3f5ab20d14ef1c29ab05c2
Jorge Barrientos <jorge.barrientos@gmail.com>
2017-07-30 22:26:45 -0600

    Store new todos on firebase using thunk

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -31,11 +31,16 @@ describe('Reducers', () => {
     it('should add new todo', () => {
       var action = {
         type: 'ADD_TODO',
-        text: 'Walk the dog'
+        todo: {
+          id: '123abv',
+          text: 'Walk the dog',
+          completed: false,
+          createdAt: 98909
+        }
       };
       var res = reducers.todosReducer(df([]), df(action));
       expect(res.length).toEqual(1);
-      expect(res[0].text).toEqual(action.text);
+      expect(res[0]).toEqual(action.todo);
     });
 
     // Define todos array with realistic todo item
